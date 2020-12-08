10e50271e3d273f2be0b8cd00399a09e01c309b5
Alexander Sobolev <sobolev.ios.developer@gmail.com>
2020-12-08 19:41:36 +0300

    Initial Commit

---
+++ Swiftbook One/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swiftbook One
+//
+//  Created by Alexander Sobolev on 08.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
